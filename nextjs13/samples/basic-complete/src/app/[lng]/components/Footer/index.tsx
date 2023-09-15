8a5d6a952d5a26a8cc6a92cee3e6f55eb594b18e
Rodolfo <mrcarromesa@gmail.com>
2023-09-15 19:21:17 -0300

    adjustments

--- nextjs13/samples/basic-complete/src/app/[lng]/components/Footer/index.tsx
+++ nextjs13/samples/basic-complete/src/app/[lng]/components/Footer/index.tsx
@@ -16,6 +16,9 @@ export const Footer = () => {
         {t('title')} - {t('test', { myVar: 'certo?' })}
       </i>
       {/* } */}
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Test
+      </button>
     </>
   )
 }
