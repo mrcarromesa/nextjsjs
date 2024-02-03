ae542f6c78afe437633f20a31981d051260f0781
Rodolfo <mrcarromesa@gmail.com>
2024-02-03 20:05:57 -0300

    chore: remove folder

--- nextjs13/samples/tailwind/src/app/contexts/hooks/useTheme.ts
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { useContext } from 'react'
-import { ThemeContext } from '../Theme'
-
-export const useTheme = () => {
-  return useContext(ThemeContext)
-}
--- nextjs13/samples/tailwind/src/worker/index.ts
+++
@@ -1,7 +0,0 @@
-// import { renderToString } from 'react-dom/server'
-
-self.onmessage = function (e) {
-  const component = e.data
-  // const code = renderToString(e.data)
-  console.log('code', component)
-}
